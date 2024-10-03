7c96e2f6ca251e5aefb41e2bce16ce7207bb1714
aarisai <80267600+aarisai@users.noreply.github.com>
2024-10-03 08:00:29 -0400

    Created main.py file

---
+++ src/main.py
@@ -0,0 +1,7 @@
+print("Main File that holds the metadata of other's Github uRL")
+print(f"""https://github.com/appu89/data_engineer
+https://github.com/JAHNAVI1202/data_engineering
+https://github.com/srikanth0099/data_engineering
+https://github.com/Lakshmikar769/Data-Engineering
+https://github.com/hemabchowdary/data_engineering
+https://github.com/majid8987/data_engineering""")
